358b70ba90e51804866d3ad689f718876f9d4a8d
ryans <ryans@jlab.org>
2022-10-17 16:49:44 -0400

    Updated schema

--- docker/oracle/setup/03_default_data.sql
+++ docker/oracle/setup/03_default_data.sql
@@ -1,9 +1,9 @@
 alter session set container = XEPDB1;
 
-insert into event_type (event_type_id, name, weight, abbreviation) values(1, 'Accelerator', 1, 'ACC');
-insert into event_type (event_type_id, name, weight, abbreviation) values(2, 'Hall A', 2, 'HLA');
-insert into event_type (event_type_id, name, weight, abbreviation) values(3, 'Hall B', 3, 'HLB');
-insert into event_type (event_type_id, name, weight, abbreviation) values(4, 'Hall C', 4, 'HLC');
-insert into event_type (event_type_id, name, weight, abbreviation) values(5, 'Hall D', 5, 'HLD');
-insert into event_type (event_type_id, name, weight, abbreviation) values(6, 'LERF', 6, 'LRF');
-insert into event_type (event_type_id, name, weight, abbreviation) values(7, 'Non-Program', 7, 'NP');
+insert into dtm_owner.event_type (event_type_id, name, weight, abbreviation) values(1, 'Accelerator', 1, 'ACC');
+insert into dtm_owner.event_type (event_type_id, name, weight, abbreviation) values(2, 'Hall A', 2, 'HLA');
+insert into dtm_owner.event_type (event_type_id, name, weight, abbreviation) values(3, 'Hall B', 3, 'HLB');
+insert into dtm_owner.event_type (event_type_id, name, weight, abbreviation) values(4, 'Hall C', 4, 'HLC');
+insert into dtm_owner.event_type (event_type_id, name, weight, abbreviation) values(5, 'Hall D', 5, 'HLD');
+insert into dtm_owner.event_type (event_type_id, name, weight, abbreviation) values(6, 'LERF', 6, 'LRF');
+insert into dtm_owner.event_type (event_type_id, name, weight, abbreviation) values(7, 'Non-Program', 7, 'NP');
