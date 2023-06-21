b0f61815dbedd42e029e3c0e7f4217b00486de81
slominskir <ryan.slominski@gmail.com>
2023-06-21 10:44:30 -0400

    Remove unused [no ci]

--- src/main/java/org/jlab/dtm/presentation/controller/operability/TrendReport.java
+++ src/main/java/org/jlab/dtm/presentation/controller/operability/TrendReport.java
@@ -1,45 +1,24 @@
 package org.jlab.dtm.presentation.controller.operability;
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.jlab.dtm.business.params.JouleReportParams;
 import org.jlab.dtm.business.params.TrendReportParams;
-import org.jlab.dtm.business.session.CategoryDowntimeFacade;
 import org.jlab.dtm.business.session.CategoryFacade;
-import org.jlab.dtm.business.session.CategoryMonthlyGoalFacade;
-import org.jlab.dtm.business.session.EventDowntimeFacade;
-import org.jlab.dtm.business.session.EventTypeFacade;
-import org.jlab.dtm.business.session.MonthlyNoteFacade;
 import org.jlab.dtm.business.session.TrendReportFacade;
-import org.jlab.dtm.business.session.OpAccHourService;
-import org.jlab.dtm.business.session.ResponsibleGroupFacade;
 import org.jlab.dtm.business.util.DtmTimeUtil;
 import org.jlab.dtm.persistence.entity.Category;
-import org.jlab.dtm.persistence.entity.EventType;
 import org.jlab.dtm.persistence.model.TrendRecord;
-import org.jlab.dtm.presentation.params.JouleReportUrlParamHandler;
 import org.jlab.dtm.presentation.params.TrendReportUrlParamHandler;
-import org.jlab.dtm.presentation.util.DtmParamConverter;
-import org.jlab.smoothness.business.util.DateIterator;
-import org.jlab.smoothness.business.util.TimeUtil;
-import org.jlab.smoothness.presentation.util.ServletUtil;
 
 /**
  *
@@ -47,23 +26,8 @@ import org.jlab.smoothness.presentation.util.ServletUtil;
  */
 @WebServlet(name = "TrendReport", urlPatterns = {"/operability/trend"})
 public class TrendReport extends HttpServlet {
-
-    @EJB
-    EventTypeFacade eventTypeFacade;
-    @EJB
-    ResponsibleGroupFacade groupFacade;
     @EJB
     CategoryFacade categoryFacade;
-    @EJB
-    CategoryDowntimeFacade categoryDowntimeFacade;
-    @EJB
-    OpAccHourService accHourService;
-    @EJB
-    EventDowntimeFacade eventDowntimeFacade;
-    @EJB
-    MonthlyNoteFacade noteFacade;
-    @EJB
-    CategoryMonthlyGoalFacade goalFacade;
     @EJB
     TrendReportFacade trendReportFacade;
 
